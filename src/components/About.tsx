504c0befafb320e76fe4ce75057ef7bb43b0d022
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2025-02-09 06:50:59 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import { Clock } from 'lucide-react';
 
 const About = () => {
   return (
-    <div className="py-20 px-4 bg-[#0D121E]">
+    <div className="py-8 px-4 bg-[#0D121E]">
       <div className="max-w-7xl mx-auto">
         <div className="grid md:grid-cols-2 gap-12 items-center">
           <div>
