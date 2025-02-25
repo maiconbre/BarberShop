f653ddcf62f174f9b497fcd5d4e77b7d1335add4
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2025-02-25 16:17:48 -0300

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { initializeMockAppointments } from '../data/mockAppointments';
 import { authenticateUser } from '../services/auth'
 
 interface AuthContextType {
