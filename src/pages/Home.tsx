2df4da9e336dae18124af3558a179552cfc99ead
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2025-03-12 19:30:43 -0300

    V0.1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import About from '../components/About';
 import Footer from '../components/Footer';
+import BookingModal from '../components/BookingModal';
 
 interface HomeProps {
   setIsModalOpen: (open: boolean) => void;
 }
 
 const Home: React.FC<HomeProps> = ({ setIsModalOpen }) => {
+  const [isBookingModalOpen, setIsBookingModalOpen] = useState(false);
+  const [selectedService, setSelectedService] = useState('');
+
+  const handleOpenBookingModal = (serviceName: string) => {
+    setSelectedService(serviceName);
+    setIsBookingModalOpen(true);
+  };
+
   return (
     <>
       <Hero setIsModalOpen={setIsModalOpen} />
 
       <div id="services">
-        <Services />
+        <Services onSchedule={handleOpenBookingModal} />
       </div>
 
       <div id="about">
@@ -24,6 +33,12 @@ const Home: React.FC<HomeProps> = ({ setIsModalOpen }) => {
       <div id="contacts" >
         <Footer />
       </div>
+
+      <BookingModal 
+        isOpen={isBookingModalOpen} 
+        onClose={() => setIsBookingModalOpen(false)} 
+        initialService={selectedService}
+      />
     </>
   );
 };
